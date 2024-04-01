a65702c11b69336a9e01e440d0c999fbae2c83b7
Bhaverohit <rohitrkade@gmail.com>
2024-04-01 21:03:11 +0530

    Initial commit

---
+++ React Projects/src/components/Accordian/data.js
@@ -0,0 +1,24 @@
+const data = [
+    {
+        id: '1',
+        question: "What are accordian components?",
+        answer: "Accordian components are user interface elements used."
+    },
+    {
+        id: '2',
+        question: "What are accordian components?",
+        answer: "Accordian components are user interface elements used."
+    },
+    {
+        id: '3',
+        question: "What are accordian components?",
+        answer: "Accordian components are user interface elements used."
+    },
+    {
+        id: '4',
+        question: "What are accordian components?",
+        answer: "Accordian components are user interface elements used."
+    }
+]
+
+export default data;
